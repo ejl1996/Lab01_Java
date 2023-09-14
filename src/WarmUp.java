72d58103612124ced3f1cf08884ad4d4535ae43e
Emma Lee <jy.lee.emma@gmail.com>
2023-09-13 18:33:59 -0700

    Implement efficient solution in main method of WarmUp.java

--- src/WarmUp.java
+++ src/WarmUp.java
@@ -1,8 +1,8 @@
 /**
  * Demonstrates good use of control structures.
  *
- * @author YOUR NAME HERE
- * @version VERSION NUMBER HERE
+ * @author
+ * @version
  */
 public class WarmUp {
 
@@ -12,6 +12,31 @@ public class WarmUp {
      * @param args command line arguments (unused)
      */
     public static void main(String[] args) {
-        // YOUR CODE GOES HERE
+        int maxBars = 0;
+        String maxBarsTime = "";
+
+        for (int hour = 1; hour <= 12; hour++) {
+            for (int minute = 0; minute <= 59; minute++) {
+                int totalBars = countBars(hour % 10) + countBars(minute / 10) + countBars(minute % 10);
+                if (totalBars > maxBars) {
+                    maxBars = totalBars;
+                    maxBarsTime = String.format("%02d:%02d", hour, minute);
+                }
+            }
+        }
+
+        System.out.println("The time with the highest number of bars: " + maxBarsTime);
+        System.out.println("The total number of bars: " + maxBars);
+    }
+
+    /**
+     * Counts the number of bars required to display a digit.
+     *
+     * @param digit an int
+     * @return bars[digit] as an int
+     */
+    private static int countBars(int digit) {
+        int[] bars = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6};
+        return bars[digit];
     }
 }
