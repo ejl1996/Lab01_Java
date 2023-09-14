14287407388e3944f1e8dc0cb36414cf21d08291
Emma Lee <jy.lee.emma@gmail.com>
2023-09-13 21:02:19 -0700

    Update Javadoc comments for public and protected methods in die.java

--- src/Die.java
+++ src/Die.java
@@ -4,7 +4,7 @@ import java.util.Random;
  * Die represents a six-sided die.
  *
  * @author BCIT
- * @author Emma Lee 
+ * @author Emma Lee
  * @version 2023
  */
 public class Die {
@@ -13,10 +13,9 @@ public class Die {
      * The number of sides.
      */
     private int faceValue;
-    private final int numberOfSides; // New instance variable, mark as final
+    private final int numberOfSides; // New instance variable, marked as final
 
-    private static final int MINIMUM_SIDES = 2; // Minimum number of sides allowed
-
+    private static final int DEFAULT_SIDES = 6; // Default number of sides
     private static final Random RANDOM_NUMBER_GENERATOR = new Random();
 
     /**
@@ -25,32 +24,54 @@ public class Die {
      * @param numberOfSides an int
      */
     public Die(int numberOfSides) {
+        final int MINIMUM_SIDES = 2; // Minimum number of sides allowed
         if (numberOfSides < MINIMUM_SIDES) {
-            throw new IllegalArgumentException("Number of sides must be at least " + MINIMUM_SIDES);
+            System.out.println("Number of sides must be at least " + MINIMUM_SIDES + ". Setting to default sides: " + DEFAULT_SIDES);
+            this.numberOfSides = DEFAULT_SIDES;
+        } else {
+            this.numberOfSides = numberOfSides;
         }
-        this.numberOfSides = numberOfSides;
         this.faceValue = 1;
     }
 
     /**
-     * Return a face value.
+     * Update the current face value of die and return the result.
+     *
+     * @return the updated face value as an int
      */
     public int roll() {
         this.faceValue = RANDOM_NUMBER_GENERATOR.nextInt(numberOfSides) + 1;
         return this.faceValue;
     }
 
+    /**
+     * Retrieve and returns the current face value of die.
+     *
+     * @return the face value as an int
+     */
     public int getFaceValue() {
         return this.faceValue;
     }
 
-    // New method to get the number of sides (accessor only)
-    public int getNumberOfSides() {
-        return this.numberOfSides;
-    }
-
+    /**
+     * Returns a String representation of this Die.
+     *
+     * @return representation as a String
+     */
     @Override
     public String toString() {
         return "Die{" + "faceValue=" + this.faceValue + "}";
     }
+
+    /**
+     * Retrieves the number of sides of the die.
+     *
+     * @return The number of sides of the die
+     */
+    @SuppressWarnings("unused")
+    public int getNumberOfSides() {
+        return this.numberOfSides;
+    }
 }
+
+
